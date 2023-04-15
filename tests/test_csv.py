834686b3f3392c8c09b7b8a645faaae3efd84b68
Lou Vaughn <ljvaughn0841@eagle.fgcu.edu>
2023-04-15 19:44:52 -0400

    Added csv test_download

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -5,11 +5,27 @@ import pytest
 import io
 import csv
 from werkzeug.datastructures import FileStorage
-    
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
+
+def test_csv_process(client):
+    csv_file = None
+
+    with open(r'tests\test_files\test1.csv', 'r') as csv_file:
+        #csv_file = FileStorage(fp)
+
+        expected_results = [[5.53, -5.39, 3, 1, 1, '2020-09-23T09:17:16.310'], 
+                            [5.53, -5.39, 29, 1, 3, '2018-09-20T13:07:46.125'], 
+                            [5.53, -5.39, 30, 1, 3, '2021-08-21T04:35:18.433']]
+        
+        actual_results = process_csv(csv_file, 7)
+
+        assert expected_results == actual_results
+
 
 def test_csv_upload(client):
     # replace with reading a csv file stored in testing data
@@ -32,16 +48,15 @@ def test_csv_upload(client):
         assert b'<div class="alert alert-danger">' not in response.data
 
 
-def test_csv_process(client):
-    csv_file = None
-
-    with open(r'tests\test_files\test1.csv', 'r') as csv_file:
-        #csv_file = FileStorage(fp)
-
-        expected_results = [[5.53, -5.39, 3, 1, 1, '2020-09-23T09:17:16.310'], 
-                            [5.53, -5.39, 29, 1, 3, '2018-09-20T13:07:46.125'], 
-                            [5.53, -5.39, 30, 1, 3, '2021-08-21T04:35:18.433']]
+def test_download(client):
+        req = client.get('/', query_string={"results": [
+             [5.53, -5.39, 3, 1, 1, '2020-09-23T09:17:16.310'], 
+             [5.53, -5.39, 30, 1, 3, '2018-09-20T13:07:46.125'], 
+             [5.53, -5.39, 42, 2, 4, '2021-08-21T04:35:18.433']]
+                                            })
         
-        actual_results = process_csv(csv_file, 7)
+        # ra, dec, sector_number, cycle, camera, obs_date
 
-        assert expected_results == actual_results
+        test_response = download()
+        assert test_response.status_code == 200
+        assert test_response.data == b"RA,Dec,Sector,Camera,Cycle,Observation Date\r\n5.53,-5.39,3,1,1,'2020-09-23T09:17:16.310'\r\n,,30,1,3,'2018-09-20T13:07:46.125'\r\n,,42,2,4,'2021-08-21T04:35:18.433'\r\n"
