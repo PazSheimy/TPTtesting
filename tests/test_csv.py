3cb611ddd73591c9ecbc7c8ed11aece0016b8057
Lou Vaughn <ljvaughn0841@eagle.fgcu.edu>
2023-04-17 15:32:46 -0400

    Fixed test_csv_process

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -22,11 +22,16 @@ def test_csv_process(client):
     with open(r'tests\test_files\test1.csv', 'r') as csv_file:
         #csv_file = FileStorage(fp)
 
-        expected_results = [[5.53, -5.39, 3, 1, 1, '2020-09-23T09:17:16.310'], 
-                            [5.53, -5.39, 29, 1, 3, '2018-09-20T13:07:46.125'], 
-                            [5.53, -5.39, 30, 1, 3, '2021-08-21T04:35:18.433']]
+        expected_results = [[5.53, -5.39, 3, 1, 1, '2018-09-20T13:07:46.125'], 
+                            [5.53, -5.39, 29, 1, 3, '2021-08-21T04:35:18.433'], 
+                            [5.53, -5.39, 30, 1, 3, '2020-09-23T09:17:16.310']]
         
         actual_results = process_csv(csv_file, 7)
+
+        print("EXPECTED RESULTS")
+        print(expected_results)
+        print("ACTUAL RESULTS")
+        print(actual_results)
 
         assert expected_results == actual_results
 
@@ -53,19 +58,19 @@ def test_csv_upload(client):
 
 
 def test_download(client):
-        req = client.get('/', query_string={"results": [
-             [5.53, -5.39, 3, 1, 1, '2020-09-23T09:17:16.310'], 
-             [5.53, -5.39, 30, 1, 3, '2018-09-20T13:07:46.125'], 
-             [5.53, -5.39, 42, 2, 4, '2021-08-21T04:35:18.433']]
-                                            })
-        
-        # ra, dec, sector_number, cycle, camera, obs_date
+    req = client.get('/', query_string={"results": [
+            [5.53, -5.39, 3, 1, 1, '2020-09-23T09:17:16.310'], 
+            [5.53, -5.39, 30, 1, 3, '2018-09-20T13:07:46.125'], 
+            [5.53, -5.39, 42, 2, 4, '2021-08-21T04:35:18.433']]
+                                        })
+    
+    # ra, dec, sector_number, cycle, camera, obs_date
 
-        test_response = download()
-        assert test_response.status_code == 200
-        assert test_response.data == b"RA,Dec,Sector,Camera,Cycle,Observation Date\r\n5.53,-5.39,3,1,1,'2020-09-23T09:17:16.310'\r\n,,30,1,3,'2018-09-20T13:07:46.125'\r\n,,42,2,4,'2021-08-21T04:35:18.433'\r\n"
-        
-        def test_invalid_csv_upload(client):
+    test_response = download()
+    assert test_response.status_code == 200
+    assert test_response.data == b"RA,Dec,Sector,Camera,Cycle,Observation Date\r\n5.53,-5.39,3,1,1,'2020-09-23T09:17:16.310'\r\n,,30,1,3,'2018-09-20T13:07:46.125'\r\n,,42,2,4,'2021-08-21T04:35:18.433'\r\n"
+    
+def test_invalid_csv_upload(client):
     csv_file = None
 
     with open(r'C:\Users\sheim\OneDrive\Desktop\testingfinalprojecttpt\TPTtesting-master\tests\test_files\invalid_test.csv', 'rb') as csv_file:
