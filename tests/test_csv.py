9f8e6623bfae03787901ceeaeb4292c126f4a012
Patrick <51823622+Pmac101@users.noreply.github.com>
2023-04-17 17:54:49 -0400

    removed selenium tests

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -5,9 +5,6 @@ import pytest
 import io
 import csv
 from werkzeug.datastructures import FileStorage
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
 
 
 @pytest.fixture
@@ -99,60 +96,3 @@ def test_no_csv_file_upload(client):
 
     # Check if the response contains an error message
     assert b'<div id="error_message" class="alert alert-danger">' in response.data
-    
-   
-# checks if error is thrown when entering only characters
-def test_error1():
-    driver = webdriver.Firefox("C:\\Users\\pmac3\\PycharmProjects\\Software Testing\\TPTtesting\\driver")
-    driver.get("http://127.0.0.1:5000/")
-    driver.maximize_window()
-
-    element = driver.find_element(By.ID, "search_input")
-    element.send_keys("sirius")
-    element = driver.find_element(By.ID, "radius")
-    element.send_keys("def")
-    element = driver.find_element(By.ID, "search_btn")
-    sleep(2)
-    element.click()
-    sleep(2)
-    element_error = driver.find_elements(By.ID, "error_message")
-    assert len(element_error) > 0
-    driver.close()
-
-
-# checks if error is thrown when entering numbers and characters
-def test_error2():
-    driver = webdriver.Firefox("C:\\Users\\pmac3\\PycharmProjects\\Software Testing\\TPTtesting\\driver")
-    driver.get("http://127.0.0.1:5000/")
-    driver.maximize_window()
-
-    element = driver.find_element(By.ID, "search_input")
-    element.send_keys("101.2872 -16.7161")
-    element = driver.find_element(By.ID, "radius")
-    element.send_keys("abc")
-    element = driver.find_element(By.ID, "search_btn")
-    sleep(2)
-    element.click()
-    sleep(2)
-    element_error = driver.find_elements(By.ID, "error_message")
-    assert len(element_error) > 0
-    driver.close()
-
-
-# checks if error is thrown when entering negative number for radius
-def test_error3():
-    driver = webdriver.Firefox("C:\\Users\\pmac3\\PycharmProjects\\Software Testing\\TPTtesting\\driver")
-    driver.get("http://127.0.0.1:5000/")
-    driver.maximize_window()
-
-    element = driver.find_element(By.ID, "search_input")
-    element.send_keys("101.2872 -16.7161")
-    element = driver.find_element(By.ID, "radius")
-    element.send_keys("-1")
-    element = driver.find_element(By.ID, "search_btn")
-    sleep(2)
-    element.click()
-    sleep(2)
-    element_error = driver.find_elements(By.ID, "error_message")
-    assert len(element_error) > 0
-    driver.close()
